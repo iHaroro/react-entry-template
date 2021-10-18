8c65f1d6e1b6db95aaa6cddd8904a6002e465183
Haroro <hutao-haroro@qq.com>
2021-10-18 15:44:19 +0800

    打包压缩配置

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 const base = require('./webpack.config')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin') // 对CSS进行压缩插件
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin')
 const pordEnvs = require('./config/env.pord')
 const { isDev } = require('./utils')
 
@@ -18,7 +18,9 @@ module.exports = merge(base, {
     }),
   ],
   optimization: {
-    minimize: true,
+    usedExports: true, // 去除未使用的导出内容等无用代码
+    minimize: true, // 压缩代码
+    concatenateModules: true, // 合并模块
     minimizer: [
       new CssMinimizerPlugin(),
       new TerserPlugin({
